689ac9844b9b496c1e43a394b5954973e1adde89
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-04-20 02:17:10 +0200

    obj export options

--- convert_blend_to_obj.py
+++ convert_blend_to_obj.py
@@ -7,9 +7,11 @@ argv = argv[argv.index("--") + 1:] # get all args after "--"
 obj_out = argv[0]
 
 # Export model space .obj
-bpy.ops.object.select_pattern(pattern='*_model', extend=False)
-bpy.ops.export_scene.obj(filepath=obj_out+'_model.obj', use_selection=True, axis_forward='-Z', axis_up='Y')
+bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.select_same_group(group='ModelSpace')
+bpy.ops.export_scene.obj(filepath=obj_out+'_model.obj', use_selection=True, axis_forward='-Z', axis_up='Y', use_triangles=True, use_uvs=True, use_materials=False)
 
 # Export scene .dao
-bpy.ops.object.select_all(action='INVERT')
+bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.select_same_group(group='WorldSpace')
 bpy.ops.export_scene.obj(filepath=obj_out+'_scene.obj', use_selection=True, axis_forward='-Z', axis_up='Y')
