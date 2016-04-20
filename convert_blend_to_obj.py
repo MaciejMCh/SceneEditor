a70ece610e0707b2c0050b8a5a8e4fec989364c2
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-04-20 02:46:22 +0200

    export scene dae

--- convert_blend_to_obj.py
+++ convert_blend_to_obj.py
@@ -7,9 +7,9 @@ argv = argv[argv.index("--") + 1:] # get all args after "--"
 obj_out = argv[0]
 
 # # Export scene .dao
-# bpy.ops.object.select_all(action='DESELECT')
-# bpy.ops.object.select_same_group(group='WorldSpace')
-# bpy.ops.export_scene.obj(filepath=obj_out+'_scene.obj', use_selection=True, axis_forward='-Z', axis_up='Y')
+bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.select_same_group(group='WorldSpace')
+bpy.ops.wm.collada_export(filepath=obj_out, selected=True)
 
 # Export models into separate .objs
 for object in bpy.data.groups['ModelSpace'].objects:
