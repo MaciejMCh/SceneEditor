7138a8ee89ed182e54c27e761d87925b954d2e03
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-04-20 21:32:00 +0200

    no ahrdcoded indices

--- process_dae.py
+++ process_dae.py
@@ -3,9 +3,12 @@ import json
 
 tree = etree.parse('Scene.dae')
 root = tree.getroot()
-renderablesRoot = root[6][0]
+namespace = '{http://www.collada.org/2005/11/COLLADASchema}'
+
+renderablesRoot = root.find(namespace + 'library_visual_scenes')[0]
 
 def toFloat(string): return float(string)
+def rotationNodeToOrientation(node): return toFloat(node.text.split(" ")[-1])
 
 renderables = []
 for renderable in renderablesRoot:
@@ -13,15 +16,15 @@ for renderable in renderablesRoot:
 	# Name
 	renderableJson['name'] = renderable.attrib['name']
 	# Mesh
-	renderableJson['mesh'] = renderable[5].attrib['url'].replace("#", "").replace("-mesh", "")
+	renderableJson['mesh'] =  renderable.find(namespace + 'instance_geometry').attrib['url'].replace("#", "").replace("-mesh", "")
 	# Geometry
 	renderableJson['geometry'] = {}
-	# Position
-	renderableJson['geometry']['position'] = map(toFloat, renderable[0].text.split(" "))
-	# Orientation
-	renderableJson['geometry']['orientation'] = map(toFloat, [renderable[1].text.split(" ")[-1], renderable[2].text.split(" ")[-1], renderable[3].text.split(" ")[-1]])
-	# Material
-	renderableJson['material'] = renderable[5][0][0][0].attrib['symbol'].replace("#", "").replace("-material", "")
+	# # Position
+	renderableJson['geometry']['position'] = map(toFloat, renderable.find(namespace + 'translate').text.split(" "))
+	# # Orientation
+	renderableJson['geometry']['orientation'] = map(rotationNodeToOrientation, renderable.findall(namespace + 'rotate'))
+	# # Material
+	renderableJson['material'] = renderable.find( './/' + namespace + 'instance_material' ).attrib['symbol'].replace("#", "").replace("-material", "")
 	renderables.append(renderableJson)
 
 
