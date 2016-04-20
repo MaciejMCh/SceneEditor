1b4ce862180563c048ba4c32f7a235a2558705b5
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-04-20 02:36:28 +0200

    export models to separate objs

--- convert_blend_to_obj.py
+++ convert_blend_to_obj.py
@@ -6,12 +6,13 @@ argv = argv[argv.index("--") + 1:] # get all args after "--"
  
 obj_out = argv[0]
 
-# Export model space .obj
-bpy.ops.object.select_all(action='DESELECT')
-bpy.ops.object.select_same_group(group='ModelSpace')
-bpy.ops.export_scene.obj(filepath=obj_out+'_model.obj', use_selection=True, axis_forward='-Z', axis_up='Y', use_triangles=True, use_uvs=True, use_materials=False)
+# # Export scene .dao
+# bpy.ops.object.select_all(action='DESELECT')
+# bpy.ops.object.select_same_group(group='WorldSpace')
+# bpy.ops.export_scene.obj(filepath=obj_out+'_scene.obj', use_selection=True, axis_forward='-Z', axis_up='Y')
 
-# Export scene .dao
-bpy.ops.object.select_all(action='DESELECT')
-bpy.ops.object.select_same_group(group='WorldSpace')
-bpy.ops.export_scene.obj(filepath=obj_out+'_scene.obj', use_selection=True, axis_forward='-Z', axis_up='Y')
+# Export models into separate .objs
+for object in bpy.data.groups['ModelSpace'].objects:
+	bpy.ops.object.select_pattern(pattern=object.name, extend=False)
+	bpy.ops.export_scene.obj(filepath=object.name+'.obj', use_selection=True, axis_forward='-Z', axis_up='Y', use_triangles=True, use_uvs=True, use_materials=False)
+	print(object.name)
