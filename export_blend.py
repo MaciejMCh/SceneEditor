ef462c478ffe3c7b72a22b45630996fbde6f1aa6
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-04-24 21:18:36 +0200

    scenes exporting
    ý5:q

--- export_blend.py
+++ export_blend.py
@@ -1,18 +1,46 @@
 import bpy
 import sys
+import json
  
 argv = sys.argv
 argv = argv[argv.index("--") + 1:] # get all args after "--"
  
 storagePath = argv[0]
 
-# # Export scene .dao
-bpy.ops.object.select_all(action='DESELECT')
-bpy.ops.object.select_same_group(group='WorldSpace')
-bpy.ops.wm.collada_export(filepath=storagePath+'scene/collada', selected=True, export_transformation_type_selection='transrotloc')
+# Read scenes
+scenesJsonArray = []
+for scene in bpy.data.scenes:
+	sceneJsonObject = {}
+	sceneJsonObject['name'] = scene.name
+	scenesJsonArray.append(sceneJsonObject)
+	objectsJsonArray = []
+	sceneJsonObject['renderables'] = objectsJsonArray
+	for ob in scene.objects:
+		objectJsonObject = {}
+		objectJsonObject['name'] = ob.name
+		objectJsonObject['mesh'] = ob.data.name
+		objectJsonObject['material'] = ob.active_material
+		objectJsonObject['transformation'] = {}
+		objectJsonObject['transformation']['position'] = [ob.location.x, ob.location.y, ob.location.z]
+		objectJsonObject['transformation']['rotation'] = [ob.rotation_euler.x, ob.rotation_euler.y, ob.rotation_euler.z]
+		objectsJsonArray.append(objectJsonObject)
+	with open(storagePath + 'scenes/' + scene.name + '.scene', 'w') as f:
+		f.write(json.dumps(sceneJsonObject))
+		f.closed
+
+# Copy all meshes to new scene and create objects in model space
+bpy.ops.scene.new(type='EMPTY')
+modelSpaceScene = bpy.context.scene
+modelSpaceScene.name = 'modelSpaceScene'
+bpy.data.screens['Default'].scene = bpy.data.scenes['modelSpaceScene']
+bpy.context.screen.scene=bpy.data.scenes['modelSpaceScene']
+for mesh in bpy.data.meshes:
+	modelSpaceObject = bpy.data.objects.new(name=mesh.name, object_data=mesh)
+	modelSpaceScene.objects.link(modelSpaceObject)
+	print(mesh)
 
 # Export models into separate .objs
-for object in bpy.data.groups['ModelSpace'].objects:
+for object in bpy.data.meshes:
 	bpy.ops.object.select_pattern(pattern=object.name, extend=False)
-	bpy.ops.export_scene.obj(filepath=storagePath+'mesh/'+object.name+'.obj', use_selection=True, axis_forward='Y', axis_up='Z', use_triangles=True, use_uvs=True, use_materials=False)
+	bpy.ops.export_scene.obj(filepath=storagePath+'meshes/'+object.name+'.obj', use_selection=True, axis_forward='Y', axis_up='Z', use_triangles=True, use_uvs=True, use_materials=False)
 	print(object.name)
--- make_assets.py
+++ make_assets.py
@@ -7,16 +7,10 @@ with open('config.json') as configurationPayload:
     configuration = json.load(configurationPayload)
 blenderFilePath = configuration['blend_file_path']
 resultOutputPath = configuration['assets_output_path']
-removeCollada = configuration['remove_collada']
 
 os.system('mkdir ' + resultOutputPath)
-os.system('mkdir ' + resultOutputPath +'/mesh')
+os.system('mkdir ' + resultOutputPath + '/meshes')
+os.system('mkdir ' + resultOutputPath + '/scenes')
 
 # Export .blend
 os.system('/Applications/blender.app/Contents/MacOS/blender ' + blenderFilePath + ' --background --python export_blend.py -- ' + resultOutputPath)
-# Process .cae
-os.system('python process_dae.py -- ' + resultOutputPath + '/scene/collada.dae')
-
-# Delete .dae
-if removeCollada:
-	os.system('rm ' + resultOutputPath + '/scene/collada.dae')
