140f1ed06cafdb82c1a6d9f2722cbb3018912a8a
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-04-25 00:56:50 +0200

    blank config

--- make_assets.py
+++ make_assets.py
@@ -7,10 +7,11 @@ with open('config.json') as configurationPayload:
     configuration = json.load(configurationPayload)
 blenderFilePath = configuration['blend_file_path']
 resultOutputPath = configuration['assets_output_path']
+blenderAppPath = configuration['blender_app_path']
 
 os.system('mkdir ' + resultOutputPath)
 os.system('mkdir ' + resultOutputPath + '/meshes')
 os.system('mkdir ' + resultOutputPath + '/scenes')
 
 # Export .blend
-os.system('/Applications/blender.app/Contents/MacOS/blender ' + blenderFilePath + ' --background --python export_blend.py -- ' + resultOutputPath)
+os.system(blenderAppPath + ' ' + blenderFilePath + ' --background --python export_blend.py -- ' + resultOutputPath)
